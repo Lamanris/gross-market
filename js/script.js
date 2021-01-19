0029d30d70608fbf69c5132531300285ce05e538
Lamanris <sprite_kg@mail.ru>
2021-01-19 15:05:14 +0400

    placemarks

--- js/script.js
+++ js/script.js
@@ -19,15 +19,13 @@ hideBtn.addEventListener('click', function () {
 let myMap;
 ymaps.ready(init);
 
-function init () {
+function init() {
     myMap = new ymaps.Map('map', {
         center: [55.754365, 37.623808], // Москва
         searchControlProvider: 'yandex#search',
         zoom: 12,
-        controls:[]
-    }),{
-
-    },
+        controls: []
+    }), {},
         // Создадим пользовательский макет ползунка масштаба.
         ZoomLayout = ymaps.templateLayoutFactory.createClass(
             "<div class='map-zoom-buttons'>" +
@@ -38,45 +36,45 @@ function init () {
             "</div>" +
             "</div>", {
 
-            // Переопределяем методы макета, чтобы выполнять дополнительные действия
-            // при построении и очистке макета.
-            build: function () {
-                // Вызываем родительский метод build.
-                ZoomLayout.superclass.build.call(this);
+                // Переопределяем методы макета, чтобы выполнять дополнительные действия
+                // при построении и очистке макета.
+                build: function () {
+                    // Вызываем родительский метод build.
+                    ZoomLayout.superclass.build.call(this);
 
-                // Привязываем функции-обработчики к контексту и сохраняем ссылки
-                // на них, чтобы потом отписаться от событий.
-                this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
-                this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this);
+                    // Привязываем функции-обработчики к контексту и сохраняем ссылки
+                    // на них, чтобы потом отписаться от событий.
+                    this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
+                    this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this);
 
-                // Начинаем слушать клики на кнопках макета.
-                $('#zoom-in').bind('click', this.zoomInCallback);
-                $('#zoom-out').bind('click', this.zoomOutCallback);
-            },
+                    // Начинаем слушать клики на кнопках макета.
+                    $('#zoom-in').bind('click', this.zoomInCallback);
+                    $('#zoom-out').bind('click', this.zoomOutCallback);
+                },
 
-            clear: function () {
-                // Снимаем обработчики кликов.
-                $('#zoom-in').unbind('click', this.zoomInCallback);
-                $('#zoom-out').unbind('click', this.zoomOutCallback);
+                clear: function () {
+                    // Снимаем обработчики кликов.
+                    $('#zoom-in').unbind('click', this.zoomInCallback);
+                    $('#zoom-out').unbind('click', this.zoomOutCallback);
 
-                // Вызываем родительский метод clear.
-                ZoomLayout.superclass.clear.call(this);
-            },
+                    // Вызываем родительский метод clear.
+                    ZoomLayout.superclass.clear.call(this);
+                },
 
-            zoomIn: function () {
-                let map = this.getData().control.getMap();
-                map.setZoom(map.getZoom() + 1, {checkZoomRange: true});
-            },
+                zoomIn: function () {
+                    let map = this.getData().control.getMap();
+                    map.setZoom(map.getZoom() + 1, {checkZoomRange: true});
+                },
 
-            zoomOut: function () {
-                let map = this.getData().control.getMap();
-                map.setZoom(map.getZoom() - 1, {checkZoomRange: true});
-            }
-        }),
+                zoomOut: function () {
+                    let map = this.getData().control.getMap();
+                    map.setZoom(map.getZoom() - 1, {checkZoomRange: true});
+                }
+            }),
 
-    zoomControl = new ymaps.control.ZoomControl({options: {layout: ZoomLayout}});
+        zoomControl = new ymaps.control.ZoomControl({options: {layout: ZoomLayout}});
 
-    myMap.controls.add(zoomControl,{
+    myMap.controls.add(zoomControl, {
         position: {
             bottom: '24px',
             right: '24px'
@@ -84,101 +82,141 @@ function init () {
     });
 
     ButtonLayout = ymaps.templateLayoutFactory.createClass([
-        '<div title="{{ data.title }}" class="map-btn">',
-        '<span class="map-btn__text">{{ data.content }}</span>',
-        '</div>'
-    ].join(''))
-    ButtonLayout1 = ymaps.templateLayoutFactory.createClass([
-        '<div title="{{ data.title }}" class="map-btn map-btn-fullscreen">',
+        '<div title="{{ data.title }}" class="map-btn map-btn-{{data.type}} {{data.isActive}}">',
         '<span class="map-btn__text">{{ data.content }}</span>',
         '</div>'
     ].join(''))
 
-        button = new ymaps.control.Button({
-            data: {
-                content: "юрлица",
-                title: "юрлица"
-            },
-            options: {
-                layout: ButtonLayout
-            }
-        });
-
-    myMap.controls.add(button, {
-        position: {
-            left: '24px',
-            bottom: '126px'
-        }
-    });
-
-    button.events.add('click', function () {
-        alert('Вы нажали на кнопку!');
-    });
-
-    button2 = new ymaps.control.Button({
+    juristicButton = new ymaps.control.Button({
         data: {
-            content: "физлица",
-            title: "физлица"
+            content: "юрлица",
+            title: "юрлица",
+            type: "juristic"
         },
         options: {
             layout: ButtonLayout
         }
     });
 
-    myMap.controls.add(button2,
-        {
-            position: {
-                bottom: '75px',
-                left: '24px'
-            }});
-
-
-    button2.events.add('click', function () {
-        alert('Вы нажали на кнопку!');
+    myMap.controls.add(juristicButton, {
+        position: {
+            left: '0',
+            bottom: '126px'
+        }
     });
 
-    var fullscreenControl = new ymaps.control.Button({
+    physicButton = new ymaps.control.Button({
         data: {
-            iconType: 'expand', title: "Развернуть/свернуть карту",
-            content: "показать всё"
+            content: "физлица",
+            title: "физлица",
+            type: "physic"
         },
         options: {
-            selectOnClick: false,
-            float: "right",
-            layout: ButtonLayout1
+            layout: ButtonLayout
         }
     });
-    fullscreenControl.events.add("press" ,function (){
-        if (!fullscreenControl.isSelected())
-            myMap.container.enterFullscreen();
-        else
-            myMap.container.exitFullscreen();
-    });
-    myMap.container.events.add("fullscreenenter" ,function() {
-        fullscreenControl.data.set({iconType:"collapse"});
-        fullscreenControl.select();
-        console.log('fullscreenenter');
-    });
-    myMap.container.events.add("fullscreenexit" ,function() {
-        fullscreenControl.data.set({iconType:"expand"});
-        fullscreenControl.deselect();
-        console.log('fullscreenexit');
-    });
-    myMap.controls.add(fullscreenControl, {
-        position: {
-            bottom: '24px',
-            left: '24px'
+
+    myMap.controls.add(physicButton,
+        {
+            position: {
+                left: '0',
+                bottom: '75px'
+            }
+        });
+
+    allButton = new ymaps.control.Button({
+        data: {
+            content: "показать всё",
+            title: "показать всё",
+            type: "all",
+            isActive: "active"
+        },
+        options: {
+            layout: ButtonLayout
         }
     });
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    )
-    function putPlacemark (coords) {
+
+    myMap.controls.add(allButton,
+        {
+            position: {
+                left: '0',
+                bottom: '24px'
+            }
+        });
+
+    juristicButton.events.add('press', function () {
+        removeMarks()
+        document.querySelectorAll('.map-btn').forEach((el) => el.classList.remove('active'))
+        document.querySelector('.map-btn-juristic').classList.add('active')
+        putPlacemark(juristicCoords, 'юрлица', 'Юридическое лицо');
+    });
+
+    physicButton.events.add('press', function () {
+        removeMarks()
+        document.querySelectorAll('.map-btn').forEach((el) => el.classList.remove('active'))
+        document.querySelector('.map-btn-physic').classList.add('active')
+        putPlacemark(physicCoords, 'физлица', 'Физическое лицо');
+    });
+
+    allButton.events.add('press', function () {
+        removeMarks()
+        document.querySelectorAll('.map-btn').forEach((el) => el.classList.remove('active'))
+        document.querySelector('.map-btn-all').classList.add('active')
+        putPlacemark(juristicCoords, 'юрлица', 'Юридическое лицо');
+        putPlacemark(physicCoords, 'физлица', 'Физическое лицо');
+    });
+
+
+
+
+    // var fullscreenControl = new ymaps.control.Button({
+    //     data: {
+    //         iconType: 'expand', title: "Развернуть/свернуть карту",
+    //         content: "показать всё"
+    //     },
+    //     options: {
+    //         selectOnClick: false,
+    //         float: "right",
+    //         layout: ButtonLayout1
+    //     }
+    // });
+    // fullscreenControl.events.add("press" ,function (){
+    //     if (!fullscreenControl.isSelected())
+    //         myMap.container.enterFullscreen();
+    //     else
+    //         myMap.container.exitFullscreen();
+    // });
+    // myMap.container.events.add("fullscreenenter" ,function() {
+    //     fullscreenControl.data.set({iconType:"collapse"});
+    //     fullscreenControl.select();
+    //     console.log('fullscreenenter');
+    // });
+    // myMap.container.events.add("fullscreenexit" ,function() {
+    //     fullscreenControl.data.set({iconType:"expand"});
+    //     fullscreenControl.deselect();
+    //     console.log('fullscreenexit');
+    // });
+    // myMap.controls.add(fullscreenControl, {
+    //     position: {
+    //         bottom: '24px',
+    //         left: '24px'
+    //     }
+    // });
+    // // Создаём макет содержимого.
+    // MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+    //     '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    // )
+
+    function removeMarks() {
+        myMap.geoObjects
+            .removeAll()
+    }
+
+    function putPlacemark(coords, type, content) {
         coords.map((el, ind) => {
             myPlacemark = new ymaps.Placemark(el, {
-                hintContent: 'Собственный значок метки',
-                balloonContent: el
+                hintContent: type,
+                balloonContent: content
             }, {
                 // Опции.
                 // Необходимо указать данный тип макета.
@@ -195,6 +233,23 @@ function init () {
                 .add(myPlacemark)
         })
     }
-    let marksCoords = [[55.757932, 37.615805], [55.753309, 37.577504],[55.765048, 37.550795],[55.739460, 37.656561],[55.732913, 37.701081],[55.761982, 37.644150],[55.765958, 37.653827],[55.778138, 37.591923],[55.769260, 37.693850],[55.783972, 37.527293] ]
-    putPlacemark(marksCoords)
-}
+
+    let juristicCoords = [[55.757932, 37.615805], [55.753309, 37.577504], [55.765048, 37.550795], [55.739460, 37.656561], [55.732913, 37.701081]]
+    let physicCoords = [[55.761982, 37.644150], [55.765958, 37.653827], [55.778138, 37.591923], [55.769260, 37.693850], [55.783972, 37.527293]]
+
+
+    putPlacemark(juristicCoords, 'юрлица', 'Юридическое лицо');
+        putPlacemark(physicCoords, 'физлица', 'Физическое лицо');
+
+    var FOCUS_ZOOM = 16;
+
+    myMap.geoObjects.events.add('click', function(e) {
+        var targetObject = e.get('target');
+
+        if (targetObject.geometry.getType() === 'Point') {
+            myMap.setCenter(targetObject.geometry.getCoordinates(), FOCUS_ZOOM, {
+                checkZoomRange: true
+            });
+        }
+    });
+    }
